6b5477115cacd5156c3604279d6f4753309d71d3
John Fiorilla <john.fiorilla@loan-street.com>
2018-05-30 17:05:36 -0400

    build

--- src/components/components.js
+++ src/components/components.js
@@ -1,9 +1,7 @@
 import DefaultComponent from './DefaultComponent';
 import Contact from './Contact';
-import Game from './Game/Game';
 
 export default {
   DefaultComponent,
-  Game,
   Contact,
 };
