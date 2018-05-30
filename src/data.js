759472ffe96cb031ec683c826e960acd4b361022
John Fiorilla <john.fiorilla@loan-street.com>
2018-05-30 17:26:06 -0400

    piecewise/responsive

--- src/data.js
+++ src/data.js
@@ -1,20 +1,17 @@
 import { serialize } from './utils';
 
 // CONTENT
-export const siteName = 'OrcPunk';
-export const email = 'hello@orcpunk.com';
+export const siteName = 'John Fio | Composer';
+export const email = 'john@audiosprite.com';
 
 export const instagramHandle = 'audiosprite';
 export const twitterHandle = 'audiosprite';
 export const soundcloudHandle = 'audiosprite';
 export const bandcampHandle = 'audiosprite';
 
-export const address = ['195 Plymouth St', 'Suite 2-9', 'Brooklyn, NY 11201'];
-export const gmapsLink = 'https://goo.gl/maps/5iRLGapdUwr';
-
-export const logoFull = 'logoFull.png';
-export const logoSquare = 'logoSquare.png';
-export const heroImg = 'heroImg.png';
+export const logoFull = '';
+export const logoSquare = '';
+export const heroImg = '';
 
 export const about = [
   'ORC PUNK is a small indie game studio in Brooklyn, NY.  Our goal is to make games that are beautiful, challenging, compelling, and fun.  We believe that games are an artform, as much for the creators as for the players, who create their own art in their expressions of play.  We approach every project from this frame.',
@@ -67,33 +64,6 @@ export const games = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis purus, vestibulum in imperdiet eu, pellentesque vel mi. Curabitur maximus ornare laoreet. Aliquam pretium velit vel laoreet aliquet. Donec a ex et mi elementum porttitor. Vestibulum id ligula purus. Aliquam hendrerit feugiat auctor.',
     gifs: [1, 2, 3, 4, 5].map(num => `HHOTR_AppVideo_1.1${num}`),
   },
-  {
-    img: 'game_card_img_hip_hop_on_the_rocks.png',
-    name: 'Hip Hop On the Rocks',
-    description:
-      'Hip-Hop On The Rocks is the hip-hop-themed Trivia party game where anything can happen!  Newly launched from Mogul Mommies, Inc, download it now and leave us a good review if you feelin us!',
-    storeLinks: [
-      {
-        platform: 'android',
-        link: 'bit.ly/playhhotrocks',
-      },
-      {
-        platform: 'ios',
-        link: 'bit.ly/hhotrocks',
-      },
-    ],
-    releaseDate: 'LAUNCHED 5/ 03/ 2018',
-    heroImg: 'carousel_img_HHOTR.png',
-    links: [
-      {
-        text: 'PressKit',
-        link: 'http://www.etc.com',
-      },
-    ],
-    youtubeLink: '',
-    gameCopy: '',
-    gifs: [1, 2, 3, 4, 5].map(num => `ITD_0.${num}`),
-  },
 ];
 
 export const pages = [
@@ -106,13 +76,6 @@ export const pages = [
     path: `/games/${serialize(game.name)}`,
     component: 'Game',
     ...game,
-  })),
-];
-
-export const homepageCarousel = [
-  ...games.map(game => ({
-    link: `games/${serialize(game.name)}`,
-    img: game.heroImg || game.img,
   })),
 ];
 
